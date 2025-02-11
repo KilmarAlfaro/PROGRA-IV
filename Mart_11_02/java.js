a69f66a2dcf755bc2ddf82f49661bdf2c697157b
KilmarAlfaro <157402339+KilmarAlfaro@users.noreply.github.com>
2025-02-11 11:51:56 -0600

    Add files via upload

---
+++ Mart_11_02/java.js
@@ -0,0 +1,31 @@
+function cambiarFondo(elemento) {
+    elemento.style.backgroundColor = "red";
+    elemento.innerHTML = " Don Napo";
+
+}
+function col(elemento){
+    elemento.innerHTML = " La musica    ";
+    elemento.style.border= "2px solid blue";
+}
+
+function cambiarTexto(elemento) {
+    elemento.innerHTML = " Ser un ingeniero";
+}
+
+function cambiarColorTexto(elemento) {
+    elemento.style.color = "blue";
+    elemento.innerHTML = " Electrónica";
+}
+
+function cambiarBorde(input) {
+    input.style.border = "3px solid blue";
+}
+
+function restaurarBorde(input){
+    input.style.border = "3px solid grey";
+}
+
+
+function alerta(){
+    alert("Don Napo")
+}
