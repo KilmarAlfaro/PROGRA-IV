ea3df84e51d967d5040ac82e651b52fc43a4f2ac
Kilmar_Alfaro <kilmaralfaro72@gmail.com>
2025-02-25 11:49:44 -0600

    parcial_1

--- scri.js
+++ scri.js
@@ -1,7 +1,57 @@
-class Componente extends HTMLElement{
-    constructor(){
-        super()
-    }
-    
 
+class OtroElemento extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML = `
+        <style>
+            p {
+                color: black;
+                font-size: 20px;
+                text-align: center;
+                margin-top: 20px;
+                font-weight: bold;
+            }
+        </style>
+        <p>PARA OBTENER MÁS INFORMACIÓN CLICKEA LAS IMÁGENES</p>
+        `;
+    }
 }
+customElements.define("mi-elemento", OtroElemento);
+
+
+class MiComponente extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+
+        const texto = this.getAttribute("texto") || "Texto por defecto";
+        this.shadowRoot.innerHTML = `
+        <style>
+            p {
+                color: black;
+                font-size: 18px;
+                text-align: center;
+                margin-top: 10px;
+                font-weight: bold;
+            }
+        </style>
+        <p>${texto}</p>
+        `;
+    }
+}
+customElements.define("mi-componente", MiComponente);
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("sunset").addEventListener("click", function() {
+        alert("Sunset Park El Salvador: Un parque de diversiones con una vista espectacular al océano.");
+    });
+
+    document.getElementById("binaes").addEventListener("dblclick", function() {
+        alert("BINAES: La Biblioteca Nacional de El Salvador, un espacio cultural y de aprendizaje moderno.");
+    });
+});
+
